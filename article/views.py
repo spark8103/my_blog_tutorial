c9bffbd929245795f65d4db6ca752443a7010872
spark8103 <chuanshuang@eastmoney.com>
2016-10-13 18:18:01 +0800

    add archives & tag..

--- article/views.py
+++ article/views.py
@@ -9,7 +9,7 @@ from django.contrib.syndication.views import Feed
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from article.models import Article
-#from article.models import Tag
+# from article.models import Tag
 
 from datetime import datetime
 
@@ -17,11 +17,50 @@ from datetime import datetime
 # Create your views here.
 def home(request):
     post_list = Article.objects.all()
-    #return HttpResponse("Hello World, Django")
-    return render(request, 'home.html', {'post_list' : post_list})
+#    return HttpResponse("Hello World, Django")
+    return render(request, 'home.html', {'post_list': post_list})
 
-def detail(request, my_args):
-    post = Article.objects.filter(id=int(my_args))[0]
-    str = ("title = %s, category = %s, date_time = %s, content = %s"
-        % (post.title, post.category, post.date_time, post.content))
-    return HttpResponse(str)
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post': post})
+
+
+def search_tag(request, tag):
+    try:
+        post_list = Article.objects.filter(category__iexact=tag)  # contains
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'tag.html', {'post_list': post_list})
+
+
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'archives.html', {'post_list': post_list,
+                                             'error': False})
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request, 'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0:
+                return render(request, 'archives.html', {'post_list': post_list,
+                                                         'error': True})
+            else:
+                return render(request, 'archives.html', {'post_list': post_list,
+                                                         'error': False})
+    return redirect('/')
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -2,12 +2,15 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
-#from article.views import RSSFeed
-#from article.views import home
+# from article.views import RSSFeed
+# from article.views import home
 from article import views as article_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', article_views.home),
-    url(r'^(?P<my_args>[0-9].*)/$', article_views.detail, name='detail'),
+    url(r'^$', article_views.home, name='home'),
+    url(r'^(?P<id>[0-9].*)/$', article_views.detail, name='detail'),
+    url(r'^archives/$', article_views.archives, name='archives'),
+    url(r'^about/$', article_views.about, name='about'),
+    url(r'^tag(?P<tag>\w+)/$', article_views.search_tag, name='search_tag'),
 ]
